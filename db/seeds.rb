1e8fd150130b4ad7f914a3390e8543e295ea4099
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2020-08-12 17:02:38 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating some flats..."
+10.times do
+  flat = Flat.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    description: Faker::Restaurant.description,
+    price_per_night: [30..42].sample,
+    number_of_guests: [2..6].sample
+  )
+
+  puts "Flat with id #{flat.id} was created"
+
+end
+
+puts "Created #{Flat.count} flats"
